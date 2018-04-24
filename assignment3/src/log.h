3c1e56452f7582e865be55f885d3075d9587d0e9
Luckysena <mykolagmd@gmail.com>
2018-04-24 00:44:06 -0400

    works ig

--- assignment3/src/log.h
+++ assignment3/src/log.h
@@ -19,6 +19,6 @@ void log_fi (struct fuse_file_info *fi);
 void log_stat(struct stat *si);
 void log_statvfs(struct statvfs *sv);
 void log_utime(struct utimbuf *buf);
-
+void log_fuse_context(struct fuse_context *context);
 void log_msg(const char *format, ...);
 #endif
